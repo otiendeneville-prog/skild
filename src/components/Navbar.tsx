981ca7222cca1b0d69211f5a1eca52e1fa26e4a2
otiendeneville-prog <otiendeneville@gmail.com>
2026-09-16 22:18:33 +0300

    import link

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,14 @@
-
+ import { Link } from "@tanstack/react-router"
 export default function Navbar() {
   return (
     <nav className="w-full bg-white justify-baseline p-5">
         <div className="brand">
-            <div className="mark">
+            <div className="m-4 p-6 bg-accent rounded-2xl shadow-md                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             ">
             <div className="glyph"/>
             </div>
+            <Link to="/">
+                <span>Skild</span>
+            </Link>
         </div>
     Navbar
     </nav>
