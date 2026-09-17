4cd9096045c0ef14125d5f1a32945d6f3d8f8118
otiendeneville-prog <otiendeneville@gmail.com>
2026-09-17 22:23:22 +0300

    rendering link

--- src/components/skillCard.tsx
+++ src/components/skillCard.tsx
@@ -1,6 +1,12 @@
+import { title } from "process";
 
-export default function skillCard() {
+export default function skillCard({authorEmail, category,createdAt,description,installCommand,tags,tittle}:SkillRecord) {
   return (
-    <div>skillCard</div>
+    <article className="relative flex flex-col h-full min-h-95 rounded-xl transion-all duration-300 border border-subtle">
+      <Link to="/skills">
+        tabIndex={-1}
+        aria-label={`open ${title}`}
+     
+      </article >
   )
 }
