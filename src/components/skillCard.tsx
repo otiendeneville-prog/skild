6b2cbfb719368729f5b8bc01a09e8620971b720f
otiendeneville-prog <otiendeneville@gmail.com>
2026-09-17 22:31:57 +0300

    overlay-class

--- src/components/skillCard.tsx
+++ src/components/skillCard.tsx
@@ -1,12 +1,13 @@
-import { title } from "process";
+import { Link } from "lucide-react"
 
-export default function skillCard({authorEmail, category,createdAt,description,installCommand,tags,tittle}:SkillRecord) {
+
+export default function skillCard({authorEmail, category,createdAt,description,installCommand,tags,title}:SkillRecord) {
   return (
     <article className="relative flex flex-col h-full min-h-95 rounded-xl transion-all duration-300 border border-subtle">
-      <Link to="/skills">
+      <Link to="/skills" />
         tabIndex={-1}
         aria-label={`open ${title}`}
-     
+        className="overlay"
       </article >
   )
 }
