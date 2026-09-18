40e0b0bb54022beebaba803c4bd0902a6582a760
otiendeneville-prog <otiendeneville@gmail.com>
2026-09-18 19:10:39 +0300

    add sinup file

---
+++ src/routes/__auth/sign-up/$.tsx
@@ -0,0 +1,19 @@
+import { createFileRoute } from '@tanstack/react-router'
+import { SignUp } from '@clerk/tanstack-react-start'
+
+export const Route = createFileRoute('/__auth/sign-up/$')({
+  component: RouteComponent,
+})
+
+function RouteComponent() {
+  return(
+     <section>
+     <SignUp
+      routing="path"
+      path="sign-up"
+      sign-upUrl="/sign-in"
+      fallbackRedirectUrl="/"
+     />
+   </section>
+  )
+}
