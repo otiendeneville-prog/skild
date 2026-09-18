572fd950685979fd2fd4a61642a78ebf522cf73c
otiendeneville-prog <otiendeneville@gmail.com>
2026-09-18 17:40:37 +0300

    import clerk from react

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -7,7 +7,7 @@ import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
 import { TanStackDevtools } from '@tanstack/react-devtools'
 import Navbar from '../components/Navbar.tsx'
 
-import ClerkProvider from '../integrations/clerk/provider'
+import { ClerkProvider } from '@clerk/tanstack-react-start'
 
 import TanStackQueryDevtools from '../integrations/tanstack-query/devtools'
 
